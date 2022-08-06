1c3d64e71099e242497fa8c8786c779d100776e5
Kimmi Lee <lee.kimmixq@gmail.com>
2022-08-07 02:31:56 +0800

    set up firebase config

---
+++ src/util/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3grs5gZ4TJs9CZPC8Xk74d_D6i6DTzqA",
+  authDomain: "just-jk-commerce.firebaseapp.com",
+  projectId: "just-jk-commerce",
+  storageBucket: "just-jk-commerce.appspot.com",
+  messagingSenderId: "316201503719",
+  appId: "1:316201503719:web:c4bee15eb1ead276884b42",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
