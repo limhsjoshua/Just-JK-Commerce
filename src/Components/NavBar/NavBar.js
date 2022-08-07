cbe072098a74588414c1c69da4d243b0dc691361
joshualhs <limhsjoshua@gmail.com>
2022-08-07 23:35:17 +0800

    Created NavBar.js, styles.js

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { CallMissedSharp, ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/jklogo.png";
+import useStyles from "./styles";
+
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Just JK Commerce"
+              height="25px"
+              className={classes.image}
+            />
+            Just JK Commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button} />
+          <Link to="products">Products </Link>
+          <Link to="checkout">Checkout</Link>
+          <IconButton aria-label="Show cart items" color="inherit">
+            <Badge badgeContent={2} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default NavBar;
