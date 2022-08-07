96ba94f580fdb84c9b02290ede43188504243954
joshualhs <limhsjoshua@gmail.com>
2022-08-07 23:35:55 +0800

    Moved links over to NavBar.js and outcommented links in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import "./App.css";
-// import Products from "./Components/Products/Products";
+import Products from "./Components/Products/Products";
 import Checkout from "./Components/Checkout/Checkout";
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
-      <div
-        id="temp-nav"
-        style={{ backgroundColor: "gray", fontWeight: "bold" }}
-      >
-        <span>Temporary Nav: </span>
-        <Link to="products">Products </Link>
-        <Link to="checkout">Checkout</Link>
+      <div>
+        <NavBar />
       </div>
       <Routes>
-        {/* <Route path="products" element={<Products />} /> */}
+        <Route path="products" element={<Products />} />
         <Route path="checkout" element={<Checkout />} />
       </Routes>
     </div>
