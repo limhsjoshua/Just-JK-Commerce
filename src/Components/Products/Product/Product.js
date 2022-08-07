1f3480bbb1daa0f5812564925ecb8d2996acaf9f
joshualhs <limhsjoshua@gmail.com>
2022-08-07 23:36:42 +0800

    Created Products folder and styles.js(for Products.js)

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -17,17 +17,19 @@ const Product = ({ product }) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} img="" title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
           <Typography variant="h5" gutterBottom>
             {product.name}
           </Typography>
-          <Typography variant="h5" gutterBottom>
-            {product.price}
-          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
         </div>
-        <Typography variant="h2" color="textSecondary">
+        <Typography variant="body2" color="textSecondary">
           {product.description}
         </Typography>
       </CardContent>
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { Grid } from "@material-ui/core/Grid";
-import Product from "../Product";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
-  { id: 1, name: "Table", description: "Wooden table", price: "$700.00" },
-  { id: 2, name: "Lamp", description: "Table lamp", price: "$90.00" },
-  { id: 3, name: "Sofa", description: "Leather sofa", price: "$3000.00" },
+  {
+    id: 1,
+    name: "Table",
+    description: "Wooden table",
+    price: "$700.00",
+    image:
+      "https://www.collinsdictionary.com/images/full/table_588358070_1000.jpg",
+  },
+  {
+    id: 2,
+    name: "Lamp",
+    description: "Table lamp",
+    price: "$90.00",
+    image:
+      "https://cdn.ambientedirect.com/chameleon/mediapool/thumbs/e/14/Artemide_Choose-Tavolo-Tischleuchte_1200x630-ID1244137-6b26b748c53db9143ef4cc7a64deb941.jpg",
+  },
+  {
+    id: 3,
+    name: "Sofa",
+    description: "Leather sofa",
+    price: "$3000.00",
+    image:
+      "https://res.cloudinary.com/blackorchid-technologies/image/upload/Geormani-3-Seater-Etchout-359",
+  },
 ];
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id}>
