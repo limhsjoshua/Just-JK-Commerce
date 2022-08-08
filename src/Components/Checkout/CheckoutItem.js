bff186518eff5d06b473f2b20e914b76d1b44fc4
Kimmi Lee <lee.kimmixq@gmail.com>
2022-08-08 14:22:51 +0800

    created checkout page

---
+++ src/Components/Checkout/CheckoutItem.js
@@ -0,0 +1,24 @@
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+
+export default function CheckoutItem({ item, idx }) {
+  const { name, price, photo, description } = item;
+  const quantity = 1;
+
+  return (
+    <TableRow>
+      <TableCell align="center">{idx + 1}</TableCell>
+      <TableCell align="center">
+        <img
+          alt={description}
+          src={photo}
+          style={{ width: "130px", height: "130px" }}
+        />
+      </TableCell>
+      <TableCell align="left">{name}</TableCell>
+      <TableCell align="center">{price}</TableCell>
+      <TableCell align="center">{quantity}</TableCell>
+      <TableCell align="center">{price * quantity}</TableCell>
+    </TableRow>
+  );
+}
