22bd351b6d00cad205a125bd480ee4414622a542
joshualhs <limhsjoshua@gmail.com>
2022-08-10 14:02:56 +0800

    added getAuth() to FIrebase,.js Passed user prop to auth and signup. Created states and event handlers for Auth and SignUp.

--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3grs5gZ4TJs9CZPC8Xk74d_D6i6DTzqA",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export default db;
+export { db, auth };
