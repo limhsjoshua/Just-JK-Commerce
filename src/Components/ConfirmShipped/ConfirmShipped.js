134707af771d8345b6f3a5d93ed3afb7cf51b6b9
Kimmi Lee <lee.kimmixq@gmail.com>
2022-08-10 14:06:47 +0800

    added different outputs depending on order status for ConfirmShipped component

--- src/Components/ConfirmShipped/ConfirmShipped.js
+++ src/Components/ConfirmShipped/ConfirmShipped.js
@@ -4,18 +4,22 @@ import { doc, getDoc, setDoc } from "firebase/firestore";
 import { Button } from "@material-ui/core";
 
 export default function ConfirmShipped({ db }) {
+  const [confirmation, setConfirmation] = useState(false);
+  const [order, setOrder] = useState(null);
+
   const location = useLocation();
 
   const orderId = location.search.split("?order=")[1];
 
-  const [order, setOrder] = useState(null);
   useEffect(() => {
     getDoc(doc(db, "orders", orderId)).then((docSnap) => {
-      if (docSnap.exists()) setOrder(docSnap.data());
+      if (docSnap.exists()) {
+        setOrder(docSnap.data());
+      } else {
+        setOrder(false);
+      }
     });
   }, []);
-
-  console.log(order);
 
   const handleClick = async () => {
     try {
@@ -23,15 +27,58 @@ export default function ConfirmShipped({ db }) {
         ...order,
         status: "shipped",
       });
+      setConfirmation(true);
     } catch (e) {
       console.error("Error updating document: ", e);
     }
   };
 
+  if (order === null) {
+    return (
+      <div style={{ marginTop: 100 }}>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (order !== null) {
+    if (order === false) {
+      return (
+        <div style={{ marginTop: 100 }}>
+          <p>Invalid Order Id</p>
+        </div>
+      );
+    }
+    if (order.status === "shipped") {
+      return (
+        <div style={{ marginTop: 100 }}>
+          <p>Order {order.id} has already been shipped!</p>
+        </div>
+      );
+    } else if (order.status !== "paid") {
+      return (
+        <div style={{ marginTop: 100 }}>
+          <p>
+            Order {order.id} still processing, should not be ready for shipping
+            yet
+          </p>
+        </div>
+      );
+    }
+  }
+
+  if (confirmation) {
+    return (
+      <div style={{ marginTop: 100 }}>
+        <p>Order status for order {order.id} updated! Thank you!</p>
+      </div>
+    );
+  }
+
   return (
     <div style={{ marginTop: 100 }}>
-      <h1>Confirm Shipping for Item?</h1>
-      <Button onClick={handleClick}>Item Shipped!</Button>
+      <h1>Confirm that these items have been shipped?</h1>
+      <Button onClick={handleClick}>Confirm</Button>
     </div>
   );
 }
