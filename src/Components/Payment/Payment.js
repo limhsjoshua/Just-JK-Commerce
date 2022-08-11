49c0326e7cb1484a4f57a3645661287a1332f11a
Kimmi Lee <lee.kimmixq@gmail.com>
2022-08-11 18:25:31 +0800

    commented out old payment component

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,89 +1,89 @@
-import { useLocation } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import emailjs from "@emailjs/browser";
-import { doc, setDoc } from "firebase/firestore";
-import axios from "axios";
-import { useState } from "react";
+// import { useLocation } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
+// import { Button } from "@material-ui/core";
+// import emailjs from "@emailjs/browser";
+// import { doc, setDoc } from "firebase/firestore";
+// import axios from "axios";
+// import { useState } from "react";
 
-export default function Payment({ db }) {
-  const location = useLocation();
-  const navigate = useNavigate();
+// export default function Payment({ db }) {
+//   const location = useLocation();
+//   const navigate = useNavigate();
 
-  const [paymentLink, setPaymentLink] = useState(null);
+//   const [paymentLink, setPaymentLink] = useState(null);
 
-  if (!location.state)
-    return (
-      <div style={{ marginTop: 100 }}>
-        <h1>Invalid page</h1>
-        <Link to="/">
-          <Button>Return to home</Button>
-        </Link>
-      </div>
-    );
+//   if (!location.state)
+//     return (
+//       <div style={{ marginTop: 100 }}>
+//         <h1>Invalid page</h1>
+//         <Link to="/">
+//           <Button>Return to home</Button>
+//         </Link>
+//       </div>
+//     );
 
-  const { order } = location.state;
+//   const { order } = location.state;
 
-  console.log(order);
+//   console.log(order);
 
-  let productsText = "";
-  order.products.forEach((product) => {
-    productsText += `${product.quantity} x ${product.name} - $${product.price} <br>`;
-  });
+//   let productsText = "";
+//   order.products.forEach((product) => {
+//     productsText += `${product.quantity} x ${product.name} - $${product.price} <br>`;
+//   });
 
-  const handlePay = async () => {
-    const res = await axios.post(
-      "http://localhost:4242/create-checkout-session",
-      { price: order.total * 100 }
-    );
-    setPaymentLink(res.data);
-  };
+//   const handlePay = async () => {
+//     const res = await axios.post(
+//       "http://localhost:4242/create-checkout-session",
+//       { price: order.total * 100 }
+//     );
+//     setPaymentLink(res.data);
+//   };
 
-  const handlePaymentSuccess = async () => {
-    try {
-      await setDoc(doc(db, "orders", order.id), {
-        ...order,
-        status: "paid",
-      });
-      emailjs
-        .send(
-          "service_9deqn3y",
-          "template_vglgj3f",
-          {
-            order_id: order.id,
-            name: order.name,
-            address: order.address,
-            email: order.email,
-            delivery: order.delivery,
-            products: productsText,
-            total: order.total,
-            link: `localhost:3000/confirm-shipped?order=${order.id}`,
-          },
-          "23GC5shygPuTYgokC"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-      navigate("/orders", { replace: true });
-    } catch (e) {
-      console.error("Error updating document: ", e);
-    }
-  };
+//   const handlePaymentSuccess = async () => {
+//     try {
+//       await setDoc(doc(db, "orders", order.id), {
+//         ...order,
+//         status: "paid",
+//       });
+//       emailjs
+//         .send(
+//           "service_9deqn3y",
+//           "template_vglgj3f",
+//           {
+//             order_id: order.id,
+//             name: order.name,
+//             address: order.address,
+//             email: order.email,
+//             delivery: order.delivery,
+//             products: productsText,
+//             total: order.total,
+//             link: `localhost:3000/confirm-shipped?order=${order.id}`,
+//           },
+//           "23GC5shygPuTYgokC"
+//         )
+//         .then(
+//           (result) => {
+//             console.log(result.text);
+//           },
+//           (error) => {
+//             console.log(error.text);
+//           }
+//         );
+//       navigate("/orders", { replace: true });
+//     } catch (e) {
+//       console.error("Error updating document: ", e);
+//     }
+//   };
 
-  return (
-    <div style={{ marginTop: 100 }}>
-      <h1>{order.id}</h1>
-      <Button onClick={handlePay}>Pay</Button>
-      {paymentLink && (
-        <a href={paymentLink} target="_blank" rel="noopener noreferrer">
-          <Button>Really Pay</Button>
-        </a>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div style={{ marginTop: 100 }}>
+//       <h1>{order.id}</h1>
+//       <Button onClick={handlePay}>Pay</Button>
+//       {paymentLink && (
+//         <a href={paymentLink} target="_blank" rel="noopener noreferrer">
+//           <Button>Really Pay</Button>
+//         </a>
+//       )}
+//     </div>
+//   );
+// }
