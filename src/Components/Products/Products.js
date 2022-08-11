3f36238d2d064562f894db2df2622d0c87c19fc8
Kimmi Lee <lee.kimmixq@gmail.com>
2022-08-11 09:31:13 +0800

    added sidebar and states for sorting and filtering

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,12 +1,29 @@
 import { useState, useEffect } from "react";
 import { Grid } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 import { collection, getDocs } from "firebase/firestore";
+import ProductsSideBar from "./ProductsSidebar/ProductsSideBar";
 
 const Products = ({ db, cart, setCart }) => {
   const classes = useStyles();
   const [products, setProducts] = useState([]);
+  const [sort, setSort] = useState("latest");
+  const [filter, setFilter] = useState({
+    Hoodies: false,
+    "T-Shirts": false,
+    "Localhost vs. Production": false,
+    "Following Tutorial Code": false,
+    "7 Hours Debugging": false,
+    "Merging Branches": false,
+    "RAM Then vs. Now": false,
+    "Learn Python in 5 Min": false,
+    "Programming is Easy": false,
+    "In Case of Fire": false,
+    "Sad Code Cat": false,
+    "It's a Feature": false,
+  });
 
   const getProducts = async () => {
     const querySnapshot = await getDocs(collection(db, "products"));
@@ -38,16 +55,35 @@ const Products = ({ db, cart, setCart }) => {
   };
 
   return (
-    <main className={classes.content}>
-      <div className={classes.toolbar} />
-      <Grid container justify="center" spacing={4}>
-        {products.map((product) => (
-          <Grid item key={product.id}>
-            <Product product={product} addToCart={addToCart} />
+    <Box style={{}}>
+      <Box
+        style={{
+          marginTop: 100,
+          padding: 10,
+          width: 200,
+          float: "left",
+        }}
+      >
+        <ProductsSideBar
+          sort={sort}
+          setSort={setSort}
+          filter={filter}
+          setFilter={setFilter}
+        />
+      </Box>
+      <Box style={{ marginLeft: 200 }}>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+              <Grid item key={product.id}>
+                <Product product={product} addToCart={addToCart} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </main>
+        </main>
+      </Box>
+    </Box>
   );
 };
 
